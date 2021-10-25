49c537dcebe34f54db09aabff0f647b33c547995
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-10-25 08:04:05 +0530

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+
+#define IO_USERNAME " Enter-here-your-adafruit-user-name "
+#define IO_KEY " Enter-here-your-adafruit-key "
+
+#define WIFI_SSID " Enter-here-your-wifi-ssid "
+#define WIFI_PASS " Enter-here-your-wifi-password "
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
